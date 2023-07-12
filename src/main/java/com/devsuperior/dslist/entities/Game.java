e3c12a1bbf284374a96132b8d765a35efaf32081
Yurilima20 <yurilima1231@gmail.com>
2023-07-11 23:34:59 -0300

    Criando projeto Spring, criando as camadas, mapeando entitades

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,25 +22,29 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre; 
-	private String platform; 
+	private String platforms; 
+	private Double score; 
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription; 
-	private String longDescriotion;
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
 	
 	public Game() {
 	} 
 	
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescription, String longDescriotion) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescriotion = longDescriotion;
+		this.longDescription = longDescription;
 	} 
 	
 	public Long getId() {
@@ -75,12 +79,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public String getImgUrl() {
@@ -99,17 +103,25 @@ public class Game {
 		this.shortDescription = shortDescription;
 	}
 
-	public String getLongDescriotion() {
-		return longDescriotion;
+	public String getLongDescription() {
+		return longDescription;
 	}
 
-	public void setLongDescriotion(String longDescriotion) {
-		this.longDescriotion = longDescriotion;
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
 	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	@Override

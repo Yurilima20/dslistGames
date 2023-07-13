16701e4e4827f2eaf2b23f989cb9bf75e3061317
Yurilima20 <yurilima1231@gmail.com>
2023-07-13 13:27:16 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -12,7 +12,7 @@ public interface GameRepository extends JpaRepository<Game, Long>{
 	
 	//nativeQuery = true deve ser colocado pelo fato de não estar usando jpql e sin sql msm
 	@Query(nativeQuery = true, value = """
-			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+			SELECT tb_game.id, tb_game.title, tb_game.game_year AS gameYear, tb_game.img_url AS imgUrl,
 			tb_game.short_description AS shortDescription, tb_belonging.position
 			FROM tb_game
 			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
